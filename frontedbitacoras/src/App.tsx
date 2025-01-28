0313367b93fd1bfb0b250ec876cc5058c61d3fee
21edclique <bryanjinez200027@gmail.com>
2025-01-28 16:24:18 -0500

    Add Front

---
+++ frontedbitacoras/src/App.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from 'react';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import DefaultLayout from './layout/DefaultLayout';
+import { Toaster } from 'react-hot-toast';
+import { useAuthContext } from './context/AuthContext';
+
+import ProtectedRoute from './utils/protectedRoute.utils';
+import { authInterceptor } from './hooks/useInterceptor';
+import Cooperatives from './pages/Registration/cooperatives.registration';
+import CooperativesList from './pages/Registration/cooperativesList.registration';
+
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
+  //user authenticated?
+  const { authUser } = useAuthContext();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  // Condición para determinar si aplicar o no DefaultLayout
+  const isAuthRoute = pathname === '/auth/signin';
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      {isAuthRoute ? (
+        <>
+          <Routes>
+        
+            <Route
+              path="/auth/signin"
+              element={
+                
+                authUser ? <Navigate to='/register/cooperativesList' /> :
+                
+                  <>
+                    <PageTitle title="SignIn | ChaskiPass" />
+                    <SignIn />
+                  </>
+              }
+            />
+          </Routes>
+          <Toaster />
+        </>
+      ) : (
+        <DefaultLayout>
+          <Routes>
+            <Route
+              path='/auth/signin'
+              element={
+                <ProtectedRoute requiredRole={['superAdmin']}>
+                  <>
+                    <PageTitle title="Routes  | ChaskiPass" />
+                    <CooperativesList />
+                  </>
+                </ProtectedRoute>
+              }
+            />
+        
+              <Route
+              path="/auth/signup"
+              element={
+
+                <ProtectedRoute requiredRole={['superAdmin']}>
+                  <>
+                    <PageTitle title="Cooperatives | ChaskiPass" />
+                    <Cooperatives/>
+                  </>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/auth/signup"
+              element={
+                <ProtectedRoute requiredRole={['superAdmin']}>
+                  <>
+                    <PageTitle title="Signin | ChaskiPass" />
+                    <SignUp />
+                  </>
+                </ProtectedRoute>
+              }
+            />
+          
+            <Route
+              path="/register/cooperativesList"
+              element={
+                <ProtectedRoute requiredRole={['superAdmin']}>
+                  <>
+                    <PageTitle title="Routes | ChaskiPass" />
+                    <CooperativesList />
+                  </>
+                </ProtectedRoute>
+              }
+            />
+         
+          </Routes>
+          <Toaster />
+        </DefaultLayout>
+      )}
+    </>
+  );
+}
+
+export default authInterceptor(App);
---
+++ frontedbitacoras/src/hooks/useSignup.ts
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { API_BASE_URL } from "../helpers/Constants";
+import toast from "react-hot-toast";
+import { verifyError } from "../helpers/VerifyErrors";
+import { UserSignUpT } from "../types";
+
+export default function useSignup() {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const login = async (userData: UserSignUpT) => {
+    setLoading(true);
+    try {
+      // Realizar la solicitud POST al servidor para el login
+      const response: Response = await fetch(`${API_BASE_URL}login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // credentials: "include", // Permite el envío de cookies (credenciales)
+        body: JSON.stringify(userData),
+      });
+
+      // Verificamos la respuesta del servidor
+      const data = await response.json();
+
+      console.log("API response:", data);
+
+      if (response.ok) {
+        // Almacena el token y los datos del usuario en localStorage
+        const { token, userData } = data;
+        if (!token || !userData) {
+          throw new Error("Datos incompletos en la respuesta del servidor.");
+        }
+
+        // Guardamos el token y los datos del usuario en localStorage
+        localStorage.setItem("chaski-token", token); // Guarda el token
+        localStorage.setItem("chaski-user", JSON.stringify(userData)); // Guarda los datos del usuario
+
+        // Actualizamos el contexto con los datos del usuario
+        setAuthUser(userData);
+        toast.success("¡Inicio de sesión exitoso!");
+      } else {
+        // Si la respuesta no es ok, lanzamos un error
+        throw new Error(data.error || "Error al iniciar sesión");
+      }
+    } catch (error) {
+      console.error("Error en el login:", error);
+      toast.error(verifyError(error));
+    } finally {
+      setLoading(false); // Restablecemos el estado de carga
+    }
+  };
+
+  return { loading, login };
+}
